8b54dcaf624e13579654ad9157600dc6d077f32b
Nguyen Viet Tien <tiennv65@viettel.com.vn>
2023-06-15 14:39:44 +0700

    Lop hoc

---
+++ src/tiennv65/SapXepGioHoc.java
@@ -0,0 +1,15 @@
+package tiennv65;
+
+import java.util.Scanner;
+
+public class SapXepGioHoc {
+    public static void main(String[] args ){
+        Scanner input = new Scanner(System.in);
+        int sohang = Integer.parseInt(input.nextLine());
+        System.out.println(input);
+    }
+
+}
+
+
+
