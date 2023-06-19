ab38583100876f810728cc38e5146a4e2b1e593c
Nguyen Viet Tien <tiennv65@viettel.com.vn>
2023-06-19 09:21:11 +0700

    Test giai thuat search

--- src/SearchClass/LinearSearch.java
+++ src/SearchClass/LinearSearch.java
@@ -1,5 +1,8 @@
 package SearchClass;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class LinearSearch {
     public static int linearSearch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
@@ -8,5 +11,23 @@ public class LinearSearch {
             }
         }
         return -1; // Trả về -1 nếu không tìm thấy
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap vao mang khong gian tim kiem: ");
+        String line = scanner.nextLine();
+        int[] items = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println("Nhap vao value can tim kiem: ");
+        int item = scanner.nextInt();
+
+        int vitri = linearSearch(items, item);
+        if (vitri==-1){
+            System.out.println("Item khong co trong mang");
+        }{
+            System.out.println("Item nam o vi tri " + vitri + " cua mang");
+        }
     }
 }
